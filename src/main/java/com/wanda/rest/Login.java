70bf3fd7710635b7fded9c943f8c5c5adb813ba2
Eisboar <sascha_hasel@yahoo.de>
2014-02-15 16:55:46 +0100

    refactoring

--- src/main/java/com/wanda/rest/Login.java
+++ src/main/java/com/wanda/rest/Login.java
@@ -1,15 +1,17 @@
-package com.wanda;
+package com.wanda.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Root resource (exposed at "myresource" path)
+ * Root resource (exposed at "myresources" path)
  */
-@Path("myresources")
-public class MyResource {
+@Path("login")
+public class Login {
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent
@@ -22,4 +24,11 @@ public class MyResource {
     public String getIt() {
         return "Got it!";
     }
+    
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String testPost(String inputJson){
+		return "Got it! "+ inputJson;
+	}
 }
